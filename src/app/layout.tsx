45440a62d9f99e9ec7050a9e7940e70051df1812
unknown <umail2bilal@gmail.com>
2025-05-30 22:14:42 +0530

    added meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,44 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Zettwise - Studio Bizkit",
-  description: "www.studiobizkit.com",
+  description: "A project by Studio Bizkit. Visit www.studiobizkit.com to learn more.",
+  keywords: ["Zettwise", "Studio Bizkit", "Web Development", "Design Agency", "Tech Project"],
+  authors: [{ name: "Studio Bizkit", url: "https://www.studiobizkit.com" }],
+  creator: "Studio Bizkit",
+  publisher: "Studio Bizkit",
+  metadataBase: new URL("https://www.studiobizkit.com"),
+  themeColor: "#ee3224",
+  openGraph: {
+    title: "Zettwise - Studio Bizkit",
+    description: "A project by Studio Bizkit. Visit www.studiobizkit.com to learn more.",
+    url: "https://www.studiobizkit.com",
+    siteName: "Studio Bizkit",
+    type: "website",
+    locale: "en_US",
+    images: [
+      {
+        url: "https://i.ibb.co/x8gTV9B8/thumbnail.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Zettwise - Studio Bizkit",
+        type: "image/jpeg",
+        secureUrl: "https://i.ibb.co/x8gTV9B8/thumbnail.jpg"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Zettwise - Studio Bizkit",
+    description: "A project by Studio Bizkit. Visit www.studiobizkit.com to learn more.",
+    site: "@StudioBizkit",
+    images: ["https://i.ibb.co/x8gTV9B8/thumbnail.jpg"]
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png"
+  },
+  manifest: "/site.webmanifest"
 };
 
 export default function RootLayout({
